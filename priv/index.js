cea3a158700cb0cd6b4a04a11ef68c2d2e07c57c
Laura Sierra <lsierragarcia4@gmail.com>
2020-12-16 00:25:14 -0500

    Arreglos del diseño

--- priv/index.js
+++ priv/index.js
@@ -4,7 +4,6 @@
  const textArea = document.querySelector("textarea").value;
 
  console.log(textArea);
- console.log("Hola papiiiii");
 
  eventListeners();
  function eventListeners(){
@@ -25,33 +24,3 @@
      }
 
   }
-     
-
-//     console.log(parser.results[0]);
-
-    // const nearley = require("nearley");
-    // const compile = require("nearley/lib/compile");
-    // const generate = require("nearley/lib/generate");
-    // const nearleyGrammar = require("nearley/lib/nearley-language-bootstrapped");
-    
-    // function compileGrammar(sourceCode) {
-    //     // Parse the grammar source into an AST
-    //     const grammarParser = new nearley.Parser(nearleyGrammar);
-    //     grammarParser.feed(sourceCode);
-    //     const grammarAst = grammarParser.results[0]; // TODO check for errors
-    
-    //     // Compile the AST into a set of rules
-    //     const grammarInfoObject = compile(grammarAst, {});
-    //     // Generate JavaScript code from the rules
-    //     const grammarJs = generate(grammarInfoObject, "grammar");
-    
-    //     // Pretend this is a CommonJS environment to catch exports from the grammar.
-    //     const module = { exports: {} };
-    //     eval(grammarJs);
-    
-    //     return module.exports;
-    // }
-    
-    // const grammar = compileGrammar("main -> foo | bar");
-    
-    // const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
