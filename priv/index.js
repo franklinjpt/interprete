8736249a20e9ae66249e37dbf717eca661cc1175
Franklin Pineda <franklinpinedat@gmail.com>
2020-12-16 01:50:38 -0500

    final5

--- priv/index.js
+++ priv/index.js
@@ -1,26 +1,41 @@
- const nearley = require("nearley");
- const grammar = require("../grammar.js");
- const formulario = document.querySelector('#formulario');
- const textArea = document.querySelector("textarea").value;
+const nearley = require("nearley");
+const grammar = require("../grammar.js");
+const formulario = document.querySelector('#formulario');
+const textArea = document.querySelector("textarea");
 
- console.log(textArea);
+eventListeners();
+function eventListeners(){
 
- eventListeners();
- function eventListeners(){
- 
-     formulario.addEventListener('submit', validarInformacion);
- 
-  }
+    formulario.addEventListener('submit', validarInformacion);
 
-  function validarInformacion(){
-    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-    
-      if(parser.results.length>1){
-        console.log("Error: gramatica ambigua detectada");
-      } else if (parser.results.length == 1) {
-        console.log(`Gramatica correcta`);
-      } else {
-        console.log("Error: parser not found.");
-     }
+ }
 
-  }
+ function validarInformacion(e){
+   e.preventDefault();
+   const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+
+   const codigo = textArea.value;
+
+   parser.feed(codigo);
+     if(parser.results.length>1){
+       Swal.fire({
+         icon: 'error',
+         title: 'Oops...',
+         text: 'Error: gramatica ambigua detectada'
+       })
+     } else if (parser.results.length == 1) {
+       Swal.fire({
+         icon: 'success',
+         title: 'Validado',
+         text: 'Gramatica correcta'
+       })
+     } else {
+       Swal.fire({
+         icon: 'error',
+         title: 'Oops...',
+         text: 'Error: gramatica incorrecta'
+       })
+       
+    }
+
+ }
